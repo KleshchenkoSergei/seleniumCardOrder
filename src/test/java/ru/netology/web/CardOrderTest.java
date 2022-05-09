60cd6579a7b84a2fe24c0b729857a2f38c7568a1
Sergei Kleshchenko <kserv2006@yandex.ru>
2022-05-09 23:09:05 +0700

    Negative tests added

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -47,7 +47,7 @@ public class CardOrderTest {
     }
 
     @Test
-    public void test() {
+    public void shouldPositiveTest() {
         driver.get("http://localhost:9999");
 
         //CSS selectors
@@ -58,4 +58,108 @@ public class CardOrderTest {
         String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
+
+    @Test
+    public void shouldTestNameLatin() {
+        driver.get("http://localhost:9999");
+
+        //CSS selectors
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Andrei");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79239236655");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"name\"] [class=\"input__sub\"]")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    public void shouldTestNameNumeric() {
+        driver.get("http://localhost:9999");
+
+        //CSS selectors
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей222");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79239236655");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"name\"] [class=\"input__sub\"]")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    public void shouldTestNameSpecialSymbol() {
+        driver.get("http://localhost:9999");
+
+        //CSS selectors
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей!");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79239236655");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"name\"] [class=\"input__sub\"]")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    public void shouldTestPhonePlusLost() {
+        driver.get("http://localhost:9999");
+
+        //CSS selectors
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("79239236655");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"phone\"] [class=\"input__sub\"]")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    public void shouldTestPhoneNumberLess() {
+        driver.get("http://localhost:9999");
+
+        //CSS selectors
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+7923923665");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"phone\"] [class=\"input__sub\"]")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    public void shouldTestPhoneNumberMore() {
+        driver.get("http://localhost:9999");
+
+        //CSS selectors
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+792392366550");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"phone\"] [class=\"input__sub\"]")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    public void shouldTestPhoneLetter() {
+        driver.get("http://localhost:9999");
+
+        //CSS selectors
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+7923923665q");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=\"phone\"] [class=\"input__sub\"]")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+    @Test
+    public void shouldTestNoAgreement() {
+        driver.get("http://localhost:9999");
+
+        //CSS selectors
+        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей");
+        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79239236655");
+        driver.findElement(By.tagName("button")).click();
+        String text = driver.findElement(By.className("input_invalid")).getText();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй", text.trim());
+    }
+
 }
