99120a21f3304743c97c1baebd46432ab798751e
Sergei Kleshchenko <kserv2006@yandex.ru>
2022-05-09 20:58:59 +0700

    Failed test

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -9,8 +9,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 import java.util.*;
 
 public class CardOrderTest {
@@ -21,7 +24,6 @@ public class CardOrderTest {
     static void setUp() {
 
         WebDriverManager.chromedriver().setup(); // использование webdriver manager
-//        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe"); //ручной выбор драйвера
     }
 
     @BeforeEach
@@ -51,22 +53,9 @@ public class CardOrderTest {
         //CSS selectors
         driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей");
         driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79239236655");
-
-        //Special CSS selectors  [data-test-id ="name"]
-//        driver.findElement(By.cssSelector("[data-test-id=\"name\"]")).sendKeys("Андрей");
-//        driver.findElement(By.cssSelector("[data-test-id=\"phone\"]")).sendKeys("+79239236655");
-
-        //No CSS selectors
-//        List<WebElement> elements = driver.findElements(By.className("input__control"));
-//        elements.get(0).sendKeys("Андрей");
-//        elements.get(1).sendKeys("+79265669999");
-
-//        driver.findElement(By.className("input_control")).sendKeys("Андрей");
-//        driver.findElement().sendKeys("+79265669999");
         driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click(); // кнопка отправить
-        //driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText(); //order-success
+        driver.findElement(By.tagName("button")).click();
         String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        assertEquals("Fail_Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
